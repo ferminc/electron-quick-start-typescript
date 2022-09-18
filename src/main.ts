3a735cbe7d086b986896c2069c7562efe2e87a79
ferminc <ferc@outlook.com>
2022-09-18 11:59:44 -0300

    test

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ function createWindow() {
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: false,
+      sandbox: true
     },
     width: 800,
   });
